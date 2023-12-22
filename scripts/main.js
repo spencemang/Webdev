5afbef7d8a0521aa6d606280a3edfb1e729b7e1d
spenceg <spencemang@gmail.com>
2023-12-22 11:14:41 -0500

    changed folder branch

---
+++ scripts/main.js
@@ -0,0 +1,39 @@
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+    const mySrc = myImage.getAttribute("src");
+    if (mySrc === "styles/images/griff1.jpg") {
+        myImage.setAttribute("src", "styles/images/lizardsmoke1.jpg");
+    } else {
+        myImage.setAttribute("src", "styles/images/griff1.jpg");
+    }
+}
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    const myName = prompt("Please enter your name:");
+    if (!myName) {
+        setUserName();
+    } else {
+    localStorage.setItem("name", myName)
+    myHeading.textContent = `Its about to get real, ${myName}`;
+}
+}
+
+if (!localStorage.getItem("name")) {
+    setUserName();
+  } else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Its about to get real, ${storedName}`;
+  }
+  
+
+
+  
+myButton.onclick = () => {
+    setUserName();
+};
+
+    
+
