4f74b8dd4ac2ff95870675439dcd8f6ed2435fbe
spenceg <spencemang@gmail.com>
2023-12-22 06:35:00 -0500

    Finished the javasript basics on MDN

--- new-test-2023/scripts/main.js
+++ new-test-2023/scripts/main.js
@@ -7,6 +7,31 @@ myImage.onclick = () => {
     } else {
         myImage.setAttribute("src", "styles/images/griff1.jpg");
     }
-    
+}
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    const myName = prompt("Please enter your name:");
+    if (!myName) {
+        setUserName();
+    } else {
+    localStorage.setItem("name", myName)
+    myHeading.textContent = `Its about to get real, ${myName}`;
+}
+
+if (!localStorage.getItem("name")) {
+    setUserName();
+  } else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Its about to get real, ${storedName}`;
+  }
+  
+
+
+  
+myButton.onclick = () => {
+    setUserName();
 };
+
 
